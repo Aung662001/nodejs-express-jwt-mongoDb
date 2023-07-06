b789a8743ebc1b91d9a1949db1070a57d4fec7bb
Aung662001 <107274438+Aung662001@users.noreply.github.com>
2023-07-06 22:57:21 +0630

    fs.createReadStream test

--- server.js
+++ server.js
@@ -1,54 +1,14 @@
 const fs = require("fs");
 const path = require("path");
 
-fs.readFile(
-  path.join(__dirname, "files", "lorem.txt"),
-  "utf-8",
-  (error, data) => {
-    if (error) throw error;
-    console.log(data);
-  }
-);
+const rs = fs.createReadStream(path.join(__dirname, "files", "lorem.txt"), {
+  encoding: "utf-8",
+});
+const ws = fs.createWriteStream(path.join(__dirname, "files", "newLorem.txt"));
 
-fs.writeFile(
-  path.join(__dirname, "files", "reply.txt"),
-  "Hello , Nice to meet You!\n",
-  (err) => {
-    if (err) throw err;
-    console.log("Write Success!");
-    //
-    fs.appendFile(
-      path.join(__dirname, "files", "reply.txt"),
-      "Nice to meet you too",
-      (err) => {
-        if (err) throw err;
-        console.log("Append success");
-      }
-    );
-    //
-    fs.rename(
-      path.join(__dirname, "files", "reply.txt"),
-      path.join(__dirname, "files", "newReply.txt"),
-      (err) => {
-        if (err) throw err;
-        console.log("Rename Complete");
-        //
-        fs.copyFile(
-          path.join(__dirname, "files", "newReply.txt"),
-          path.join(__dirname, "files", "a.txt"),
-          (err) => {
-            if (err) throw err;
-            console.log("Copy Success");
-            fs.unlink(path.join(__dirname, "files", "newReply.txt"));
-          }
-        );
-      }
-
-      //
-    );
-  }
-);
-
+rs.on("data", (chunk) => {
+  ws.write(chunk);
+});
 process.on("uncaughtException", (err) => {
   console.error(`There is an uncaught error`, err);
   process.exit(1);
