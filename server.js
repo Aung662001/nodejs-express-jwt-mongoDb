e0a9efd401855f25c2e2d2c051d17f27ed007278
Aung662001 <107274438+Aung662001@users.noreply.github.com>
2023-07-06 23:02:38 +0630

    mkdir rmdir test

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const fs = require("fs");
 const path = require("path");
 
-const rs = fs.createReadStream(path.join(__dirname, "files", "lorem.txt"), {
-  encoding: "utf-8",
-});
-const ws = fs.createWriteStream(path.join(__dirname, "files", "newLorem.txt"));
+// const rs = fs.createReadStream(path.join(__dirname, "files", "lorem.txt"), {
+//   encoding: "utf-8",
+// });
+// const ws = fs.createWriteStream(path.join(__dirname, "files", "newLorem.txt"));
 
-rs.on("data", (chunk) => {
-  ws.write(chunk);
-});
+if (!fs.existsSync("./new")) {
+  fs.mkdir("./new", (err) => {
+    if (err) throw err;
+    console.log("Dirtory Created");
+  });
+} else {
+  fs.rmdir("./new", (err) => {
+    if (err) throw err;
+    console.log("Diretory Deleted");
+  });
+}
+// rs.pipe(ws);
 process.on("uncaughtException", (err) => {
   console.error(`There is an uncaught error`, err);
   process.exit(1);
